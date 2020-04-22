094c7c7d1cd92e23d8310e1d46f8048e1a09e869
Aarón RJ <aresinodeveloper@gmail.com>
2020-04-22 14:53:32 +0200

    feat: add tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <button class="border rounded bg-green-600 text-white">Botón</button>
   </div>
 </template>
 
