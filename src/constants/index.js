2f74ec4448f4ff7f4661f40ae83c98b2d5ae250a
Aarón RJ <aresinodeveloper@gmail.com>
2020-05-05 13:54:29 +0200

    Adding files.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import { ROUTES } from '@/constants/routes'
 
-const URL_API = process.env.VUE_APP_URL_API
+const URL_API = process.env.URL_API || 'http://192.168.1.75:3000/'
 
 export { ROUTES, URL_API }
