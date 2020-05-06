8d0300040fc3ab45d5484c79d30677f091a323f3
Aarón RJ <aresinodeveloper@gmail.com>
2020-05-06 07:55:31 +0200

    Feat: Server.js added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(express.static(__dirname + '/dist/'))
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+app.listen(port)
